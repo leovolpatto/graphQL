3e589e77b7df3a4c6fb0cf6d137223611dce6183
leovolpatto <leovolpatto@gmail.com>
2019-11-26 11:37:59 -0300

    adding simple query

--- NodeJS/graphQl_apis/src/resolvers.ts
+++ NodeJS/graphQl_apis/src/resolvers.ts
@@ -1,5 +1,24 @@
+class friend{
+  constructor(public age: Number, public name: String, public gender: string){
+
+  }
+}
+
 export default {
     Query: {
-      testMessage: (): string => 'Hello World!',
+      testMessage: (): string => 'This is a Leo\'s emergency broadcast system!',
+
+      friends: (pageSize : Number = 20): any => {
+        let friends = [];
+        friends.push(new friend(25, 'Ricks\'', 'M'));
+        friends.push(new friend(25, 'Marceleza\'', 'M'));
+        friends.push(new friend(25, 'Vitin\'', 'M'));        
+        friends.push(new friend(25, 'China', 'M'));
+        friends.push(new friend(30, 'Diego', 'M'));
+        friends.push(new friend(25, 'Willbur', 'M'));
+        friends.push(new friend(32, 'Gi', 'F'));
+        
+        return friends;
+      }
     },
   };
--- NodeJS/graphQl_apis/src/type-defs.ts
+++ NodeJS/graphQl_apis/src/type-defs.ts
@@ -2,9 +2,14 @@ import { gql } from 'apollo-server';
 
 export default gql`
   type Query {
-    """
-    Test Message. 
-    """
     testMessage: String!
+    friends(
+      pageSize: Int
+    ): [Friend]!
+  }
+
+  type Friend {
+    name: String
+    age: Int
   }
 `;
